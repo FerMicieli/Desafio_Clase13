0e2b81a09b51483c6f604062db4603a7620ea4a2
Fernando Fabrizio Micieli <micielifernando@hotmail.com>
2020-09-27 13:35:51 -0300

    Primer commit

---
+++ Operacion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESAFÍOCLASE13
+{
+    static class Operacion
+    {
+        public static int SUMAR(int num1, int num2)
+        {
+            int resultado;
+
+            resultado = num1 + num2;
+
+            return resultado;
+        }
+
+        public static int RESTAR(int num1, int num2)
+        {
+            int resultado;
+
+            resultado = num1 - num2;
+
+            return resultado;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESAFÍOCLASE13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, resultadosuma, resultadoresta;
+            String respuesta;
+
+            Console.WriteLine("Bienvenido al programa para restar y sumar 2 numeros enteros.");
+            do
+            {
+                Console.WriteLine("Ingrese el primer número entero:");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese el segundo número entero:");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                resultadosuma = Operacion.SUMAR(num1, num2);
+                resultadoresta = Operacion.RESTAR(num1, num2);
+                Console.WriteLine("El resultado de  " + num1 + " + " + num2 + " es: " + resultadosuma);
+                Console.WriteLine("El resultado de " + num1 + " - " + num2 + " es: " + resultadoresta);
+
+                Console.WriteLine("Ingrese S si quiere volver a ingresar 2 números, en caso contrario presione otra tecla.");
+                respuesta = Console.ReadLine();
+                Console.Clear();
+            } while (respuesta == "S" || respuesta == "s");
+
+            Console.WriteLine("Adiós, vuelva prontos...");
+            Console.ReadKey();
+        }
+    }
+}
